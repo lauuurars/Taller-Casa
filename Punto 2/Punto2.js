53905b9ad1b5ee90858617a3f49d8f910aa16314
lauuurars <1120098177@u.icesi.edu.co>
2025-03-25 15:21:03 -0500

    fin punto 2

--- Punto 2/Punto2.js
+++ Punto 2/Punto2.js
@@ -1,1 +1,29 @@
-let sabor = prompt("Ingrese el sabor de la pizza que desea");
+
+// nombrar variables
+let sabor = prompt("Ingrese el sabor de la pizza que desea:");
+let tamaño = prompt("Ingrese el tamaño que desea (Grande, Mediana, Personal):");
+let deseabebida = prompt("¿Desea agregar bebida? (Si/No):");
+let bebida = "";
+
+if (deseabebida === "Si"|| deseabebida == "si" || deseabebida == "SI") {
+    bebida = prompt("Ingrese la bebida que desea:");
+}
+
+let mensaje = "Confirmar pedido: ";
+mensaje += "Pizza: " + sabor;
+mensaje += ", Tamaño: " + tamaño;
+
+if (deseabebida === "Si"|| deseabebida == "si" || deseabebida == "SI") {
+    mensaje += ", Bebida: " + bebida;
+} else{
+    mensaje += ", Sin Bebida";
+}
+
+//confirmación de pedido
+let confirmar = confirm(mensaje + " ¿Desea confirmar el pedido? (Si/No):");
+
+if (confirmar) {
+    console.log("Pedido Confirmado");
+} else {
+    console.log("Pedido Cancelado");
+}
